c7e5f290205c16e72974bc9e07577d41de6a8334
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2018-02-27 15:38:14 -0300

    Exercicios até ficha da academia com mensagens do imc

---
+++ funcoes.js
@@ -0,0 +1,39 @@
+function devolveUmNumeroDecimal( pergunta ) {
+	var numero = parseFloat(prompt( pergunta ).replace(",", "."));
+	return numero;
+}
+function formataSaida( valor , numeroCasas ) {
+	return valor.toFixed(numeroCasas).replace(".", ",");
+}
+function formataGrana(valor) {
+	return formataSaida(valor, 2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function carregaCss() {
+	var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'http://cb2a5e94.ngrok.io/basico.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
+carregaCss();
